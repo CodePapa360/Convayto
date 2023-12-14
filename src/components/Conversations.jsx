ce94a7da8a7409f439514db778a88d684dfa474d
codepapa360 <codepapa360@gmail.com>
2023-12-15 00:18:45 +0600

    testing prefetching

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -2,14 +2,43 @@ import styled from "styled-components";
 import { useConversatoins } from "../features/converse/useConversations";
 import Conversation from "./Conversation";
 import { sortConverseByTime } from "../utils/common";
+import { useUser } from "../features/authentication/useUser";
+import { getMessages } from "../services/apiAuth";
+import { useQueryClient } from "@tanstack/react-query";
 
 function Conversations() {
   const { data: conversations, isPending } = useConversatoins();
+  const { user } = useUser();
+  const queryClient = useQueryClient();
 
   const sortedConversations =
     conversations?.length > 1
       ? conversations?.sort(sortConverseByTime)
       : conversations;
+
+  // prefetching messages of other conversations
+  sortedConversations?.slice(0, 5).forEach((conv) => {
+    const { friend } = conv;
+    const { id: friendUserId } = friend;
+    const myUserId = user?.id;
+
+    const test = async () => {
+      // The results of this query will be cached like a normal query
+      await queryClient.prefetchQuery({
+        queryKey: ["friend", friendUserId],
+        queryFn: () => getMessages({ myUserId, friendUserId }),
+      });
+    };
+
+    test();
+  });
+
+  // const { data, error } = useQuery({
+  //   queryKey: ["friend", friendUserId],
+  //   queryFn: () => getMessages({ myUserId, friendUserId }),
+  //   // Use previousData to update the messages array
+  //   select: (data) => ({ ...data, messages: [...(data?.messages || [])] }),
+  // });
 
   return (
     <StyledConversations>
