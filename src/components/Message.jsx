28df31420461c065c76ba82581e1eee8377d1b6a
codepapa360 <codepapa360@gmail.com>
2024-01-07 23:44:20 +0600

    Removed unnecessary classes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,9 +8,9 @@ function Message({ message }) {
     <div
       className={`${
         message?.sender_id === user.id
-          ? "bg-darkViolet dark:bg-darkViolet-dark self-end rounded-br-none text-white"
+          ? "self-end rounded-br-none bg-darkViolet text-white dark:bg-darkViolet-dark"
           : "rounded-bl-none bg-mediumSlate dark:bg-mediumSlate-dark"
-      } w-fit max-w-[80%] rounded-2xl bg-mediumSlate px-4 py-2 dark:bg-mediumSlate-dark`}
+      } w-fit max-w-[80%] rounded-2xl px-4 py-2 `}
     >
       <p>
         {message?.content}
