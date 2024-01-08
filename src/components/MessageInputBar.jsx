2291574e7f7817181a2810f9fe6b4d288ef252f3
codepapa360 <codepapa360@gmail.com>
2024-01-08 17:33:49 +0600

    Refactor MessageInputBar styling

--- src/components/MessageInputBar.jsx
+++ src/components/MessageInputBar.jsx
@@ -74,7 +74,7 @@ function MessageInputBar() {
   }
 
   return (
-    <div className="px-4 pb-4 pt-2">
+    <div className="px-4 py-2">
       <form className="mx-auto grid max-w-3xl grid-cols-[1fr_auto] overflow-hidden rounded-full  border border-borderShade bg-lightSlate dark:border-borderShade-dark dark:bg-lightSlate-dark">
         <input
           className="h-12 w-full bg-transparent pl-4 pr-2 outline-none"
