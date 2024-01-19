8c168ba6b6410694eea28153097f4f4f797ebafc
codepapa360 <codepapa360@gmail.com>
2024-01-19 21:32:19 +0600

    Add password reset functionality to Profile component

--- src/components/Profile/ChangePasssword.jsx
+++ src/components/Profile/ChangePasssword.jsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
 import { useUser } from "../../features/authentication/useUser";
 import { sendPasswordResetEmail } from "../../services/apiAuth";
+import Loader from "../Loader";
 
 function ChangePasssword() {
   const { user } = useUser();
   const { email } = user;
 
-  function handleRequest() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  async function handleRequest() {
+    setIsLoading(true);
+
     try {
-      sendPasswordResetEmail(email);
+      const data = await sendPasswordResetEmail(email);
+      if (data) {
+        setIsLoading(false);
+        setIsSuccess(true);
+      }
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   }
 
   return (
     <button
+      disabled={isLoading || isSuccess}
       onClick={handleRequest}
-      className="mt-6 h-10 rounded-md bg-textViolet px-4 text-lightSlate hover:bg-textViolet/50"
+      className="mt-6 flex h-10 items-center gap-2 rounded-md bg-textViolet px-4 text-lightSlate hover:bg-textViolet/50 disabled:bg-gray-500/30 disabled:opacity-70"
     >
-      Reset password
+      {!isSuccess ? (
+        <>
+          <span>Reset password</span>
+          {isLoading && <Loader />}
+        </>
+      ) : (
+        <span>Sent email</span>
+      )}
     </button>
   );
 }
