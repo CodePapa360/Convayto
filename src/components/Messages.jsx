9dfa1901628905f2283ccdb548e344b3f02abb75
codepapa360 <codepapa360@gmail.com>
2024-01-25 23:11:50 +0600

    Add scrollToBottom functionality to Messages component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -3,12 +3,14 @@ import Message from "./Message";
 import { useRef } from "react";
 import { sortMessageByTime } from "../utils/common";
 import Loader from "./Loader";
+import { scrollToBottom } from "../utils/common";
 
 function Messages() {
   const { data, isPending } = useMessages();
   const messages = data?.messages.sort(sortMessageByTime);
 
   const bottomRef = useRef();
+  bottomRef.current && scrollToBottom(bottomRef);
 
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-col gap-2 px-4 pt-2">
@@ -25,11 +27,6 @@ function Messages() {
         <Message message={message} key={message.id} />
       ))}
 
-      {bottomRef?.current?.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      })}
       <span ref={bottomRef}></span>
     </div>
   );
