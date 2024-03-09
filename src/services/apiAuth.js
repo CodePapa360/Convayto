a9695b6d139145998db3149d2fd32e1dc99ed0e6
codepapa360 <codepapa360@gmail.com>
2024-03-09 22:32:20 +0600

    Remove console.log statements

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -208,7 +208,6 @@ export async function sendMessage({
 
   if (conversationError) throw new Error(conversationError.message);
 
-  // console.log("message successfully sent", data[0]);
   return data[0];
 }
 
