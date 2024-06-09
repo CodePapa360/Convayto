dc3e28bb7f6259dd95a52a41362f72ad752f28c7
alamin <codepapa360@gmail.com>
2024-06-09 00:39:52 -0700

    Refactor: restored file to fix git issues

---
+++ src/features/sideBar/apiConversation.js
@@ -0,0 +1,48 @@
+import supabase from "../../services/supabase";
+
+export async function getConversationEntries({ myUserId }) {
+  const { data, error } = await supabase
+    .from("conversations")
+    .select("*, messages(*)")
+    .or(`user1_id.eq.${myUserId},user2_id.eq.${myUserId}`);
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export async function getConversations({ myUserId }) {
+  const data = await getConversationEntries({ myUserId });
+
+  // Extract friend IDs
+  const friendsIds = data.map((frnd) =>
+    frnd.user1_id === myUserId ? frnd.user2_id : frnd.user1_id,
+  );
+
+  // Fetch user data from Supabase
+  const { data: users, error } = await supabase
+    .from("users")
+    .select("*")
+    .in("id", friendsIds);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  // Create a mapping object to associate friend IDs with user data
+  const usersMapping = {};
+  users.forEach((user) => {
+    usersMapping[user.id] = user;
+  });
+
+  const combinedArray = data.map((msg) => {
+    const friendId = msg.user1_id === myUserId ? msg.user2_id : msg.user1_id;
+    const user = usersMapping[friendId];
+
+    return {
+      friend: user,
+      ...msg,
+    };
+  });
+
+  return combinedArray;
+}
