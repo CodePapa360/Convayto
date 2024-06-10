5a16a7b2fea550b0b1d748bcb54f3f624679b0e9
alamin <codepapa360@gmail.com>
2024-06-09 23:50:41 -0700

    Refactor: Update useSendNewMessage to include optimistic updates for sending new messages

--- src/features/messageArea/useSendNewMessage.js
+++ src/features/messageArea/useSendNewMessage.js
@@ -1,9 +1,57 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { sendMessage } from "./apiMessage";
+import useConvInfo from "./useConvInfo";
 
 export function useSendNewMessage() {
+  const queryClient = useQueryClient();
+  const { convInfo, isPending, isError } = useConvInfo();
+
+  const conversation_id = convInfo?.id;
+  const friendUserId = convInfo?.friendInfo?.id;
+
   const { mutate: sendNewMessage, isPending: isSending } = useMutation({
     mutationFn: sendMessage,
+
+    // When mutate is called:
+    onMutate: async (newMessage) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({
+        queryKey: ["friend", friendUserId, conversation_id],
+      });
+
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData([
+        "friend",
+        friendUserId,
+        conversation_id,
+      ]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(
+        ["friend", friendUserId, conversation_id],
+        (oldMessages) => {
+          // console.log(oldMessages);
+
+          return {
+            ...oldMessages,
+            pages: oldMessages?.pages
+              .slice()
+              .map((page, index) =>
+                index === 0
+                  ? !page
+                    ? [newMessage]
+                    : [...page, newMessage]
+                  : page,
+              ),
+          };
+        },
+      );
+
+      // Return a context object with the snapshotted value
+      return { previousTodos };
+    },
+
     onError: (err) => console.log(err.message),
   });
 
