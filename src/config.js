a4fc87158dfaf6b7f0ab22cef425c4df9e36a707
alamin <codepapa360@gmail.com>
2024-06-20 14:31:39 -0700

    refactor: Update form validation and input handling in Signup component

--- src/config.js
+++ src/config.js
@@ -2,9 +2,15 @@ export const DARK_THEME = "dark";
 export const LIGHT_THEME = "light";
 export const LOCAL_STORAGE_KEY = "theme";
 export const MAX_BIO_LENGTH = 140;
-export const MAX_NAME_LENGTH = 25;
+export const MAX_NAME_LENGTH = 70;
 export const MIN_USERNAME_LENGTH = 4;
-export const MAX_USERNAME_LENGTH = 20;
+export const MAX_USERNAME_LENGTH = 30;
+export const MIN_PASSWORD_LENGTH = 6;
 export const MINIMUM_SEARCH_LENGTH = 2;
 export const MAX_PREFETCHED_CONVERSATIONS = 0;
 export const MAX_MESSAGES_PER_PAGE = 20;
+
+export const USERNAME_REGEX = /^[a-z0-9_-]+$/;
+export const NAME_REGEX = /^(?!.*\s{2})[a-zA-Z0-9 ]+$/;
+export const EMAIL_REGEX =
+  /^[^\W_]+\w*(?:[.-]\w*)*[^\W_]+@[^\W_]+(?:[.-]?\w*[^\W_]+)*(?:\.[^\W_]{2,})$/;
--- src/features/authentication/Signup.jsx
+++ src/features/authentication/Signup.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useSignup } from "./useSignup";
 import Loader from "../../components/Loader";
 import Heading from "../../components/Heading";
@@ -7,68 +6,174 @@ import FormContainer from "../../components/FormContainer";
 import InputBox from "../../components/InputBox";
 import SubmitBtn from "../../components/SubmitBtn";
 import TextLink from "../../components/TextLink";
+import { Controller, useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  EMAIL_REGEX,
+  MAX_NAME_LENGTH,
+  MAX_USERNAME_LENGTH,
+  MIN_PASSWORD_LENGTH,
+  MIN_USERNAME_LENGTH,
+  NAME_REGEX,
+  USERNAME_REGEX,
+} from "../../config";
 
 function Signup() {
-  const [fullname, setFullname] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("123456789");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm({
+    defaultValues: {
+      fullname: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
   const { signup, isPending } = useSignup();
+  const navigate = useNavigate();
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  useEffect(() => {
+    const isAuthenticated = false;
+    if (isAuthenticated) {
+      navigate("/", { replace: true });
+    }
+  }, [navigate]);
 
-    signup(
-      { fullname, username, email, password },
-      {
-        onSettled: () => {
-          setFullname("");
-          setUsername("");
-          setEmail("");
-          setPassword("");
-        },
-      },
-    );
-  }
+  const onSubmit = ({ fullname, username, email, password }) => {
+    const cleanFullname = fullname.trim();
+    const cleanUsername = username.trim();
+    const cleanEmail = email.trim();
+
+    signup({
+      fullname: cleanFullname,
+      username: cleanUsername,
+      email: cleanEmail,
+      password,
+    });
+  };
 
   return (
     <MainContainer>
-      <FormContainer onSubmit={handleSubmit}>
+      <FormContainer onSubmit={handleSubmit(onSubmit)}>
         <Heading addClass="text-3xl">Sign up</Heading>
 
-        <InputBox
-          value={fullname}
-          onChange={setFullname}
-          placeholder="Full name"
-          htmlFor="fullname"
+        <Controller
+          name="fullname"
+          control={control}
+          rules={{
+            required: "Name is required",
+            pattern: {
+              value: NAME_REGEX,
+              message: "Only letters, numbers, and single spaces are allowed",
+            },
+            minLength: {
+              value: 1,
+              message: `Minimum ${1} characters required`,
+            },
+            maxLength: {
+              value: MAX_NAME_LENGTH,
+              message: `Maximum ${MAX_NAME_LENGTH} characters allowed`,
+            },
+          }}
+          render={({ field }) => (
+            <InputBox
+              type="text"
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={() => trigger("fullname")}
+              placeholder="Full name"
+              htmlFor="fullname"
+              error={errors.fullname?.message}
+            />
+          )}
         />
 
-        <InputBox
-          value={username}
-          onChange={setUsername}
-          placeholder="Username"
-          htmlFor="username"
+        <Controller
+          name="username"
+          control={control}
+          rules={{
+            required: "Username is required",
+            pattern: {
+              value: USERNAME_REGEX,
+              message:
+                "Only lowercase letters, numbers, underscores, and dashes are allowed",
+            },
+            minLength: {
+              value: MIN_USERNAME_LENGTH,
+              message: `Minimum ${MIN_USERNAME_LENGTH} characters required`,
+            },
+            maxLength: {
+              value: MAX_USERNAME_LENGTH,
+              message: `Maximum ${MAX_USERNAME_LENGTH} characters allowed`,
+            },
+          }}
+          render={({ field }) => (
+            <InputBox
+              type="text"
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={() => trigger("username")}
+              placeholder="Username"
+              htmlFor="username"
+              error={errors.username?.message}
+            />
+          )}
         />
 
-        <InputBox
-          value={email}
-          onChange={setEmail}
-          placeholder="Email"
-          htmlFor="email"
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: "Email is required",
+            pattern: {
+              value: EMAIL_REGEX,
+              message:
+                "Invalid email address. Please enter a valid email address",
+            },
+          }}
+          render={({ field }) => (
+            <InputBox
+              type="email"
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={() => trigger("email")}
+              placeholder="email"
+              htmlFor="email"
+              error={errors.email?.message}
+            />
+          )}
         />
 
-        <InputBox
-          value={password}
-          onChange={setPassword}
-          placeholder="Password"
-          htmlFor="password"
-          type="password"
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: MIN_PASSWORD_LENGTH,
+              message: `Weak password. Minimum ${MIN_PASSWORD_LENGTH} characters required`,
+            },
+          }}
+          render={({ field }) => (
+            <InputBox
+              type="password"
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={() => trigger("password")}
+              placeholder="Password"
+              htmlFor="password"
+              error={errors.password?.message}
+            />
+          )}
         />
 
-        <SubmitBtn disabled={isPending}>
-          {isPending && <Loader size="small" />}
-          <span className="ml-2">Sign up</span>
+        <SubmitBtn disabled={isPending} type="submit">
+          {isPending ? <Loader size="small" /> : "Sign up"}
         </SubmitBtn>
 
         <p>
