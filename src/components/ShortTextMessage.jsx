621ce8641f29cdc54e8c25dfbe1f75898ecc6630
alamin <codepapa360@gmail.com>
2024-07-01 00:18:03 +0600

    style: Fix layout issues in the Messages component

--- src/components/ShortTextMessage.jsx
+++ src/components/ShortTextMessage.jsx
@@ -1,7 +1,7 @@
 function ShortTextMessage({ children, opacity = 60 }) {
   return (
     <span
-      className={`slideUpMessage my-4 flex items-center justify-center opacity-${opacity}`}
+      className={`slideUpMessage my-4 flex items-center justify-center opacity-${opacity} select-none`}
     >
       {children}
     </span>
--- src/features/messageArea/Messages.jsx
+++ src/features/messageArea/Messages.jsx
@@ -72,40 +72,42 @@ function Messages() {
     );
 
   return (
-    <div
-      tabIndex={-1}
-      className="mx-auto grid w-full max-w-3xl grid-cols-1 grid-rows-[auto] items-end overflow-y-auto px-4"
-    >
-      {pages && !pages[0] && (
-        <ShortTextMessage>No messages yet</ShortTextMessage>
-      )}
+    <div className="grid grid-cols-1 items-end overflow-y-auto">
+      <div
+        tabIndex={-1}
+        className="mx-auto flex w-full max-w-3xl flex-col px-4"
+      >
+        {pages && !pages[0] && (
+          <ShortTextMessage>No messages yet</ShortTextMessage>
+        )}
 
-      {pages && pages[0] && (
-        <>
-          {hasNextPage && (
-            <span ref={topRef}>{isFetchingNextPage && <Loader />}</span>
-          )}
+        {pages && pages[0] && (
+          <>
+            {hasNextPage && (
+              <span ref={topRef}>{isFetchingNextPage && <Loader />}</span>
+            )}
 
-          {pages.map((page, index) =>
-            page.length ? (
-              <span key={index} className="flex w-full flex-col">
-                {page.map((message) => (
-                  <MessageItem key={message.id} message={message} />
-                ))}
+            {pages.map((page, index) =>
+              page.length ? (
+                <span key={index} className="flex w-full flex-col">
+                  {page.map((message) => (
+                    <MessageItem key={message.id} message={message} />
+                  ))}
 
-                {index === 0 && <span ref={lastPageBtm}></span>}
-              </span>
-            ) : (
-              <span
-                key={index}
-                className="mx-auto my-4 h-2 w-2 select-none  rounded bg-lightSlate opacity-60 dark:bg-lightSlate-dark"
-              ></span>
-            ),
-          )}
-        </>
-      )}
+                  {index === 0 && <span ref={lastPageBtm}></span>}
+                </span>
+              ) : (
+                <span
+                  key={index}
+                  className="mx-auto my-4 h-2 w-2 select-none  rounded bg-lightSlate opacity-60 dark:bg-lightSlate-dark"
+                ></span>
+              ),
+            )}
+          </>
+        )}
 
-      <span ref={bottomRef}></span>
+        <span ref={bottomRef}></span>
+      </div>
     </div>
   );
 }
