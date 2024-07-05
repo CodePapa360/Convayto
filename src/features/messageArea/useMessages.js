bcedf3d94ac9f6bcbcee6386dcfd51bc6f6a08e2
alamin <codepapa360@gmail.com>
2024-07-05 17:26:02 +0600

    refactor: Extract message subscription logic into separate hook

--- src/features/messageArea/useMessages.js
+++ src/features/messageArea/useMessages.js
@@ -1,8 +1,8 @@
 import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { getMessages } from "./apiMessage";
-import { useEffect, useRef } from "react";
-import { subscribeRealtimeMessage } from "./apiRealtimeMessage";
+import { useEffect } from "react";
 import useConvInfo from "./useConvInfo";
+import useMessageSubscription from "./useMessageSubscription";
 
 export function useMessages() {
   const {
@@ -57,64 +57,8 @@ export function useMessages() {
     enabled: !!friendUserId,
   });
 
-  /////////////////////////////
   // Realtime subscription //
-  /////////////////////////////
-
-  const subscriptionRef = useRef(null);
-
-  useEffect(
-    function () {
-      if (!conversation_id) return;
-
-      if (subscriptionRef.current) subscriptionRef.current.unsubscribe();
-
-      function callback(newData) {
-        queryClient.setQueryData(["friend", friendUserId], (prevData) => {
-          const existingOptimisticMessage = prevData?.pages[0]?.find(
-            (message) => message?.optimistic,
-          );
-
-          if (existingOptimisticMessage) {
-            // replace existing optimistic message with server new message
-            return {
-              ...prevData,
-              // replace the new message to the first page's data which id matches the optimistic message
-              pages: prevData.pages
-                .slice()
-                .map((page, index) =>
-                  index === 0
-                    ? page.map((message) =>
-                        message.id === newData.id ? newData : message,
-                      )
-                    : page,
-                ),
-            };
-          } else {
-            return {
-              ...prevData,
-              // add the new message to the first page's data
-              pages: prevData.pages.slice().map((page, index) => {
-                return index === 0 ? [...page, newData] : page;
-              }),
-            };
-          }
-        });
-      }
-
-      subscriptionRef.current = subscribeRealtimeMessage({
-        conversation_id,
-        callback,
-      });
-
-      return () => {
-        subscriptionRef.current?.unsubscribe();
-        subscriptionRef.current = null;
-        // console.log("unsubscribed message");
-      };
-    },
-    [conversation_id, friendUserId, queryClient],
-  );
+  useMessageSubscription({ conversation_id, friendUserId });
 
   const isLoading = isPendingConvInfo || isLoadingMessages;
 
