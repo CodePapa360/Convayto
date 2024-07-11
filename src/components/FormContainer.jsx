76969a6d092c8a2f8d65743d0f7d8f0ab1412859
alamin <codepapa360@gmail.com>
2024-07-11 17:03:37 +0600

    Style: Wrap form in a div for to add some padding around

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,10 +1,12 @@
 export default function Form({ children, onSubmit }) {
   return (
-    <form
-      onSubmit={onSubmit}
-      className="flex w-full max-w-sm flex-col rounded-xl bg-bgPrimary p-6 shadow-2xl dark:bg-bgTertiary"
-    >
-      {children}
-    </form>
+    <div className=" flex w-full items-center justify-center p-4">
+      <form
+        onSubmit={onSubmit}
+        className="flex w-full max-w-sm flex-col rounded-xl bg-bgPrimary p-6 shadow-2xl dark:bg-bgTertiary"
+      >
+        {children}
+      </form>
+    </div>
   );
 }
