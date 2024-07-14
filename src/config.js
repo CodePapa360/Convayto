57f586ad78989376ce2944789091082d4af76217
alamin <codepapa360@gmail.com>
2024-07-14 21:43:29 +0600

    refactor: organized config.js

--- src/config.js
+++ src/config.js
@@ -2,15 +2,13 @@
 export const REDIRECT_URL_LOCAL = "http://localhost:3000";
 export const REDIRECT_URL_PRODUCTION = "https://chat-su.vercel.app";
 
-//name of the app
 export const APP_NAME = "Convayto";
-
-// A nice default bio for new users which will use the app name
 export const DEFAULT_BIO = `Hey there! I'm using ${APP_NAME}`;
-
 export const DARK_THEME = "dark";
 export const LIGHT_THEME = "light";
 export const LOCAL_STORAGE_KEY = "theme";
+
+// Lengths and limits for various fields
 export const MAX_BIO_LENGTH = 140;
 export const MAX_NAME_LENGTH = 70;
 export const MIN_USERNAME_LENGTH = 4;
@@ -20,6 +18,7 @@ export const MINIMUM_SEARCH_LENGTH = 2;
 export const MAX_PREFETCHED_CONVERSATIONS = 5;
 export const MAX_MESSAGES_PER_PAGE = 20;
 
+// Regex for various fields
 export const USERNAME_REGEX = /^[a-z0-9_-]+$/;
 export const NAME_REGEX = /^(?!.*\s{2})[a-zA-Z0-9 ]+$/;
 export const EMAIL_REGEX =
