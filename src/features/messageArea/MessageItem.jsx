b5cf98025651cc88a682345411c3ae925c577cfc
alamin <codepapa360@gmail.com>
2024-07-31 21:36:42 +0600

    Add message tail to chat bubbles using :before pseudo-element

--- src/features/messageArea/MessageItem.jsx
+++ src/features/messageArea/MessageItem.jsx
@@ -6,11 +6,11 @@ function MessageItem({ message }) {
 
   return (
     <div
-      className={`${
+      className={`relative ${
         message?.sender_id === user.id
-          ? "self-end rounded-br-none bg-gradient-to-br from-bgAccent to-bgAccentDim text-textPrimary-dark dark:from-bgAccent-dark dark:to-bgAccentDim-dark"
-          : "rounded-bl-none bg-bgPrimary dark:bg-LightShade/20"
-      } my-1 w-fit max-w-[80%] rounded-2xl px-4 py-2 shadow-md`}
+          ? "self-end rounded-br-none bg-gradient-to-br from-bgAccent to-bgAccentDim text-textPrimary-dark before:absolute before:bottom-0 before:right-0 before:h-0 before:w-0 before:translate-x-full before:border-l-8 before:border-t-8 before:border-l-bgAccentDim before:border-t-transparent before:content-[''] dark:from-bgAccent-dark dark:to-bgAccentDim-dark before:dark:border-l-bgAccentDim-dark"
+          : "rounded-bl-none bg-bgPrimary before:absolute before:bottom-0 before:left-0 before:h-0 before:w-0 before:-translate-x-full before:border-r-8 before:border-t-8 before:border-r-bgPrimary before:border-t-transparent before:content-[''] dark:bg-LightShade/20 before:dark:border-r-LightShade/20"
+      } my-1 w-fit max-w-[80%] rounded-2xl px-4 py-2 shadow-md before:shadow-md`}
     >
       <p>
         {message?.content}
